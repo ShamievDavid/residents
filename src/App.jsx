57b84a590a4b7c59924b28a6452b89a72d31fcbe
david.shamiiev <david.shamiiev@rad4m.com>
2023-03-06 18:28:37 +0200

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { Main } from './features/Main';
 import { Card } from './features/Card/Card';
@@ -7,37 +7,6 @@ import { residents } from './data';
 function App() {
   const [blur, setBlur] = useState(false);
   const [selected, setSelected] = useState(null);
-
-  useEffect(() => {
-    let lastScrollY = 0;
-    let ticking = false;
-
-    const onScroll = () => {
-      lastScrollY = window.scrollY;
-      console.log('scroll', lastScrollY);
-
-      if (!ticking) {
-        window.requestAnimationFrame(() => {
-          hideAddressBar();
-          ticking = false;
-        });
-
-        ticking = true;
-      }
-    };
-
-    const hideAddressBar = () => {
-      if (window.scrollY > lastScrollY) {
-        window.scrollTo(0, 1);
-      }
-    };
-
-    window.addEventListener('scroll', onScroll, { passive: true });
-
-    return () => {
-      window.removeEventListener('scroll', onScroll);
-    };
-  }, []);
 
   const handleClose = () => {
     setSelected(null);
